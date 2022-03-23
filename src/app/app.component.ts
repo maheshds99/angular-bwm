1c890c99df9cdd6bc704fd9df43414f25ef10564
maheshds99 <86165118+maheshds99@users.noreply.github.com>
2022-03-23 17:38:54 +0530

    fetaure branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bwm-ng';
+  componentTitle = 'i am conponent';
+
+  clickHandler(){
+    alert("i M CLICKED")
+  }
 }

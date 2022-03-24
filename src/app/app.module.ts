d42a0990226c4d478863b36747836a1534e4278f
maheshds99 <86165118+maheshds99@users.noreply.github.com>
2022-03-24 12:43:41 +0530

    adds rental list component,adds rental  detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalModule } from './rental/rental.module';
+import { TempComponent } from './temp/temp.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TempComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RentalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
